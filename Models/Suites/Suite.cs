ef9dfd30594c325521783e8de84efbd1975f3e54
valeriasouza28 <valeriasouza28@gmail.com>
2024-05-10 18:18:09 -0300

    finaliza

--- Models/Suites/Suite.cs
+++ Models/Suites/Suite.cs
@@ -6,6 +6,13 @@ namespace DesafioProjetoHospedagem.Models.Suites
     public class Suite
     {
         public Suite() { }
+        public string TipoSuite { get; set; }
+        public int Capacidade { get; set; }
+        public decimal ValorDiaria { get; set; }
+        public int NumeroSuite { get; set; }
+        public StatusSuite Status { get; set; } = StatusSuite.Disponivel;
+        public Hotel hotel { get; set; } = new Hotel();
+        // Suite suite;
 
         public Suite(string tipoSuite, int capacidade, decimal valorDiaria, int numeroSuite)
         {
@@ -29,63 +36,13 @@ namespace DesafioProjetoHospedagem.Models.Suites
                 throw new ArgumentException("O número da suíte deve ser maior que zero.");
             }
 
+
+
             TipoSuite = tipoSuite;
             Capacidade = capacidade;
             ValorDiaria = valorDiaria;
             NumeroSuite = numeroSuite;
             Status = StatusSuite.Disponivel;
-        }
-
-        public string TipoSuite { get; set; }
-        public int Capacidade { get; set; }
-        public decimal ValorDiaria { get; set; }
-        public int NumeroSuite { get; set; }
-        public StatusSuite Status { get; set; } = StatusSuite.Disponivel;
-        public List<Suite> Suites { get; set; } = new List<Suite>();
-        public Hotel Hotel { get; set; }
-
-        public void CadastrarSuite()
-        {
-            Suite suite = new Suite();
-            if (suite == null)
-            {
-                throw new ArgumentNullException(nameof(suite), "A suíte fornecida para cadastro não pode ser nula.");
-            }
-
-            if (Suites.Contains(suite))
-            {
-                throw new InvalidOperationException("A suíte já está cadastrada.");
-            }
-
-            if (Suites.Count >= Hotel.QtdSuites)
-            {
-                throw new InvalidOperationException("Todas as suítes já estão cadastradas. Consulte o hotel para a quantidade de suítes disponíveis.");
-            }
-
-            Suites.Add(suite);
-        }
-
-        public void RemoverSuite(Suite suite)
-        {
-            if (suite == null)
-            {
-                throw new ArgumentNullException(nameof(suite), "A suíte fornecida para remoção não pode ser nula.");
-            }
-
-            if (!Suites.Contains(suite))
-            {
-                throw new InvalidOperationException("A suíte não está cadastrada e não pode ser removida.");
-            }
-
-            Suites.Remove(suite);
-        }
-
-        public void ListarSuites(List<Suite> suites)
-        {
-            foreach (var s in suites)
-            {
-                Console.WriteLine(s);
-            }
         }
 
         public bool VerificaDisponibilidadeDaSuite()
@@ -103,9 +60,10 @@ namespace DesafioProjetoHospedagem.Models.Suites
             Status = StatusSuite.Disponivel;
         }
 
-        public override string ToString()
+
+        public void ToString()
         {
-            return $"Nome: {TipoSuite}, Capacidade: {Capacidade}, Preço: R${ValorDiaria}, Número: {NumeroSuite}, Status: {Status}";
+            Console.WriteLine($"Nome: {TipoSuite}, Capacidade: {Capacidade}, Preço: R${ValorDiaria}, Número: {NumeroSuite}, Status: {Status}");
         }
     }
 }
